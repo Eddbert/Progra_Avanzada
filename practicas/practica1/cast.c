a546e0afe296fe2f219ff9637a5f27a97e80fcb8
Eddbert <eddyargott@hotmail.com>
2018-01-18 20:49:29 -0600

    Practica 1

---
+++ practicas/practica1/cast.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int a = 250;
+	int j = 0,k = 0,l = 0;
+	int *p;
+	long int d = (long)a * a;
+	float f = 4.67;
+	int e = 4;
+	int c = f + e;
+	p = &a;
+	*p = 400;
+	p++;
+	*p = 5555;
+	//printf("a =  %p, d =  %ld, f = %f, e = %d, c = %d \n",&a,d,f,e,c);
+	printf(" a = %d, p=%p, j = %d, k = %d, l = %d \n",a,p,j,k,l);
+	return 0;
+}
